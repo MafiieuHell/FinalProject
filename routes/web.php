9d060a5c62639c95975c37cd3062b91262d291d7
Owen <houessouowen@gmail.com>
2021-05-18 12:30:50 +0000

    modif code

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [App\Http\Controllers\DefaultController::class, 'home'])->name('home');
-<<<<<<< HEAD
 Route::get('/posts',[App\Http\Controllers\PostController::class,'index'])->name('posts.index');
 Route::get('/posts/{slug}',[App\Http\Controllers\PostController::class,'show'])->name('posts.show');
-=======
 Route::get('/users/register', [App\Http\Controllers\UserController::class, 'register'])->name('register');
 Route::get('/users/login', [App\Http\Controllers\UserController::class, 'login'])->name('login');
 Route::post('/users/signin', [App\Http\Controllers\UserController::class, 'signin'])->name('signin');
-Route::get('/', [App\Http\Controllers\DefaultController::class, 'home'])->name('home');
-Route::get('/posts',[App\Controllers\PostController::class,'index'])->name('index');
 
->>>>>>> 16aca07910b46fa07a2ae3202d78259f05a66b41
